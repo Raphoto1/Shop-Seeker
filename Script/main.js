9b0f727c5fe4f82f45847c66698e23cfbde31a3f
Rafael Martinez <rrhhmmtt@gmail.com>
2022-09-17 12:21:47 -0500

    se agregan cards desde js

--- Script/main.js
+++ Script/main.js
@@ -1,7 +1,71 @@
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
+let designs = [];
+let likedList= [];
 
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
-console.log("aqui estoy");
+class Design{
+    constructor(id,photo,title,text,style,shop1,shop2,shop3){
+        this.id = id;
+        this.photo = photo;
+        this.title = title;
+        this.text = text;
+        this.style = style;
+        this.shop1 = shop1;
+        this.shop2 = shop2;
+        this.shop3 = shop3;
+    }
+    deployDesigns(){
+        const card =`
+        <div class="col">
+                <div class="card h-100">
+                    <img src=${this.photo} class="card-img-top designImage">
+                    <div class="card-Body">
+                        <h5 class="card-title">${this.title}</h5>
+                        <p class="card-text">${this.text}</p>
+                        <p class="card-text">${this.style}</p>
+                        <a href="#" id=${this.id} class="btn btn-primary">add to List</a>
+                        <div id="shopList${this.id}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        const groupCards = document.getElementById (`groupCards`);
+        groupCards.innerHTML += card;  
+    }
+    
+    deployShops(){
+        let shop1Ava = checkShops(this.shop1);
+        let shop2Ava = checkShops(this.shop2);
+        let shop3Ava = checkShops(this.shop3);
+        const shops =`
+        ${shop1Ava}
+        ${shop2Ava}
+        ${shop3Ava}
+        `
+        const shopList = document.getElementById(`shopList${this.id}`);
+        shopList.innerHTML += shops;
+    }
+}
+
+let design1 = new Design(`001`,`"/assets/Img/Flamingo.jpg"`,`Flamingo`,`love flamingos`,`Digital`,`redbubble.com`,`society6.com`,``);
+let design2 = new Design(`002`,`"/assets/Img/Flamingo.jpg"`,`Flamingo`,`love flamingos`,`Digital`,`redbubble.com`,``,`displate.com`);
+let design3 = new Design(`003`,`"/assets/Img/Flamingo.jpg"`,`Flamingo`,`love flamingos`,`Digital`,``,`society6.com`,``);
+
+designs.push(design1,design2,design3);
+
+designs.forEach(e => {
+    e.deployDesigns();
+    e.deployShops();
+});
+
+function checkShops(shop){
+    let link;
+    if(shop !== ""){
+        console.log(`en check ${shop}`);
+        link =`
+        <a href="${shop}" class="btn">shop cargada ${shop}</a>
+        `
+    }else{
+        link = ``;
+    }
+    return link;
+}
